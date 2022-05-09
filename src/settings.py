3bed3c58ffae60f09a0dc0409eb4e9c9db463d0a
Alexander Maru <i@alexandermaru.com>
2022-05-09 02:44:20 +0000

    disable debug mode

--- src/settings.py
+++ src/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-%u0=d2m!ctor#8z3_%(x8rj64xoo2@*xkt+xc9vb0j%+7_#^8f'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 CORS_ALLOW_ALL_ORIGINS = True
