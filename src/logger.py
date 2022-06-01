1675cbae439b1e36c3325718f63c0d8f97c84cd2
Alexander Maru <i@alexandermaru.com>
2022-06-01 08:24:07 +0000

    emit default error log level and cleanup

--- src/logger.py
+++ src/logger.py
@@ -1,7 +1,7 @@
 def log_context(request, response, errors, received_at):
+    print("\n", flush=True)
     print("Started POST \"/api\" at " + str(received_at), flush=True)
     print("  Request: " + request, flush=True)
     print("  Response: " + response, flush=True)
     if len(errors) > 0:
         print("  Errors: " + ", ".join(errors), flush=True)
-    print("\n", flush=True)
